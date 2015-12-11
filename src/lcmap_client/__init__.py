95fa087e76c13f594f2f2fdab7c8143fc095ca9d
Duncan McGreggor <oubiwann@gmail.com>
2015-12-11 12:13:56 -0600

    Removed kludge for old circular dependency work-around.

--- src/lcmap_client/__init__.py
+++ src/lcmap_client/__init__.py
@@ -1,18 +1,7 @@
 import logging
 
+from lcmap_client.client import Client
+
 
 log = logging.getLogger(__name__)
-
-
-def Client(*args, **kwargs):
-    from lcmap_client.client import Client
-    return Client(*args, **kwargs)
-
-
-def get_version():
-    from lcmap_client.http import client_version
-    return client_version
-
-
-__version__ = get_version()
-
+__version__ = "0.1.0"
