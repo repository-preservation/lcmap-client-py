4d894bc73a73fa5419109b8cd6eb4ed4e3c28dd5
Duncan McGreggor <oubiwann@gmail.com>
2015-12-11 12:15:50 -0600

    Internal HTTP client updates.

--- src/lcmap_client/http.py
+++ src/lcmap_client/http.py
@@ -19,14 +19,15 @@ import lcmap_client
 
 
 log = logging.getLogger(__name__)
-context = "/api"
-server_version = "1.0"
-client_version = "0.1.0"
 default_endpoint = "http://localhost:8080"
 default_content_type = "json"
 vendor = "vnd.usgs.lcmap"
 project_url = "https://github.com/usgs-eros/lcmap-client-py"
-user_agent = "LCMAP REST Client/{} (Python {}) (+{})".format(
+
+
+def get_user_agent ():
+    from lcmap_client import __version__ as client_version
+    return "LCMAP REST Client/{} (Python {}) (+{})".format(
     client_version, sys.version.replace("\n", ""), project_url)
 
 
@@ -49,7 +50,7 @@ def format_accept(vendor, version, content_type):
 
 
 def get_base_headers():
-    return {"User-Agent": user_agent}
+    return {"User-Agent": get_user_agent()}
 
 
 def make_headers(version, content_type):
@@ -78,6 +79,12 @@ class Response(object):
     def follow_link(self):
         return self.http.get(self.get_link())
 
+    @property
+    def data(self):
+        return {"result": self.result,
+                "errors": self.errors,
+                "status": self.status_code}
+
 
 class HTTP(object):
     def __init__(self, cfg=None):
@@ -90,8 +97,8 @@ class HTTP(object):
             content_type=self.cfg.get_content_type()))
 
     def set_auth(self, auth):
-        self.auth = auth
         self.session.headers.update(make_auth_header(auth.get_token()))
+        self.auth = auth
 
     def request(self, method, path, **kwargs):
         url = self.base_url + path
@@ -103,17 +110,19 @@ class HTTP(object):
         #raw_resp = self.session.send(req.prepare())
         errors = []
         if raw_resp:
-            resp = raw_resp.json()
-            errors.extend(resp.get("errors") or [])
+            log.debug("Got raw response: {}".format(raw_resp))
+            resp = self.get_body(raw_resp)
+            log.debug("JSON for response: {}".format(resp))
+            errors = self.update_errors(errors, resp)
         else:
             resp = None
         if raw_resp.status_code == 404:
             msg = "Resource not found."
-            log.error(msg)
             errors.append(msg)
         # XXX we'll need to change the following to parse the results based on
         # content type, but right now the server is returning the fairly useless
         # application/octet-stream type, even for JSON
+        self.log_errors(errors)
         return Response(self, raw_resp, resp.get("result"), errors)
 
     def post(self, path, **kwargs):
@@ -127,3 +136,22 @@ class HTTP(object):
 
     def delete(self, path,**kwargs):
         return self.request('DELETE', path, **kwargs)
+
+    def get_body(self, raw_resp):
+        resp = raw_resp.json()
+        if "body" in resp:
+            resp = resp.get("body")
+        return resp
+
+    def update_errors(self, errors, resp):
+        if "error" in resp:
+            error = resp.get("error")
+            if error:
+                errors.append(error)
+        if "errors" in resp:
+            errors.extend(resp.get("errors") or [])
+        return errors
+
+    def log_errors(self, errors):
+        for error in errors:
+            log.error(error)
