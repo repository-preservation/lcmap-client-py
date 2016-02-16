37722fa66e2ce218491aa753f39d331711400a82
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-02-15 20:57:36 -0600

    Fixed cli tool name.

--- setup.py
+++ setup.py
@@ -47,13 +47,10 @@ setup(
         "Operating System :: POSIX",
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
+        #"Programming Language :: Python :: Implementation :: PyPy",
         "Topic :: Utilities",
     ],
     keywords=[
@@ -65,5 +62,5 @@ setup(
     },
     entry_points='''
         [console_scripts]
-        lcmap-client=lcmap_client.cli:cli
+        lcmap=lcmap_client.cli:cli
     ''')
