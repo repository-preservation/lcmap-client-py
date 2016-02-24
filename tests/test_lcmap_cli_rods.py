63566261c83175b8d535fdf94fc7e088a1816ed4
Duncan McGreggor <dmcgreggor@usgs.gov>
2016-02-24 11:04:49 -0600

    Added new unit tests for unscaled data.

--- tests/test_lcmap_cli_rods.py
+++ tests/test_lcmap_cli_rods.py
@@ -20,5 +20,20 @@ class RodResultTestCase(unittest.TestCase):
             self.assertEqual(parsed, expected.read().strip())
 
 
+class RodResult3YearsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        try:
+            fh = open("tests/data/cli-rod-3yrs-results.pkl", "rb")
+            self.results = pickle.load(fh)
+        finally:
+            fh.close()
+
+    def test_parse_as_text(self):
+        with open("tests/data/cli-rod-3yrs-results.txt", "r") as expected:
+            parsed = query.parse_to_text(self.results)
+            self.assertEqual(parsed, expected.read().strip())
+
+
 if __name__ == '__main__':
     unittest.main()
