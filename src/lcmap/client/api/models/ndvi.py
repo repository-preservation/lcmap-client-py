37132fa9054033831361427983f39c97cf6f9dcf
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-06-10 10:33:45 -0500

    Fix NDVI model run URL

--- src/lcmap/client/api/models/ndvi.py
+++ src/lcmap/client/api/models/ndvi.py
@@ -12,4 +12,4 @@ class NDVI(base.APIComponent):
 
     def run(self, x, y, t1, t2):
         params = {"x": x, "y": y, "t1": t1, "t2": t2}
-        return self.http.post(self.context, data=params)
+        return self.http.post(context, data=params)
