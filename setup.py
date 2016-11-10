e2cedfc05542c3e45bccc6a454c022b2a50574b9
clay austin <clay.austin.ctr@usgs.gov>
2016-11-10 15:15:56 -0600

    rename src dir, strip gdal related funcs from setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 # -*- encoding: utf-8 -*-
 import glob
-import re
 import io
 import sys
-from sys import version_info
 from os.path import basename
 from os.path import splitext
 from setuptools import find_packages
@@ -19,7 +17,7 @@ class Tox(TestCommand):
         self.test_suite = True
 
     def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
+        # import here, cause outside the eggs aren't loaded
         import tox
         errcode = tox.cmdline(self.test_args)
         sys.exit(errcode)
@@ -30,49 +28,6 @@ def read(filename, codec=None):
         return handle.read()
 
 
-def min_gdal_version():
-    """Returns the installed gdal version or Exception if not installed."""
-
-    cmd = ['gdal-config', '--version']
-
-    if version_info >= (2,4) and version_info <= (2,6):
-        from subprocess import Popen, PIPE
-        p = Popen(cmd, stdout=PIPE)
-        version = p.communicate()[0].strip()
-    elif version_info >=(2,7):
-        from subprocess import check_output
-        version = check_output(cmd).strip()
-    else:
-        raise Exception('Unsupported Python version:{0}.{1}.{2}'
-            .format(version_info.major,
-                    version_info.minor,
-                    version_info.micro))
-
-    if (type(version) is not str):
-        version = version.decode('utf-8')
-
-    return version
-
-
-def max_gdal_version():
-    ''' Returns the maximum pygdal version that can be installed '''
-    #parts = min_gdal_version().decode('utf-8').split('.')
-    parts = min_gdal_version().split('.')
-    if len(parts) == 3:
-        parts.append('999')
-        max_version = '.'.join(parts)
-    elif len(parts) > 3:
-        max_version = '.'.join(parts)
-    else:
-        raise Exception('Can\'t determine max gdal version from {0}'
-            .format(min_gdal_version()))
-
-    if (type(max_version) is not str):
-        max_version = max_version.decode('utf-8')
-
-    return max_version
-
-
 setup(
     name='lcmap-client',
     version='1.0.0-dev',
@@ -80,15 +35,15 @@ setup(
     description='LCMAP REST Service Client (Python)',
     long_description='{0}'.format(read('README.md')),
     author='USGS EROS',
-    author_email='http://eros.usgs.gov',
+    author_email='custserv@usgs.gov',
     url='https://github.com/usgs-eros/lcmap-client-py',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    namespace_packages = ['lcmap'],
+    packages=find_packages('lcmap_client'),
+    package_dir={'': 'lcmap_client'},
+    namespace_packages=['lcmap'],
 
     # py_modules is an alternative way to look for what's included.
     # packages should handle it.  WIP
-    py_modules=[splitext(basename(i))[0] for i in glob.glob('src/*.py')],
+    py_modules=[splitext(basename(i))[0] for i in glob.glob('lcmap_client/*.py')],
 
     include_package_data=True,
     zip_safe=False,
@@ -107,23 +62,15 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities',
     ],
-    keywords=[
-        # eg: "keyword1", "keyword2", "keyword3",
-    ],
-    install_requires=['six', 'requests', 'pylru', 'termcolor', 'nose',
-                      'click', 'DateTime',
-                      'pygdal>={0},<={1}'.format(min_gdal_version(),
-                                                 max_gdal_version()),
-                      'pandas'
-    ],
+    keywords=['lcmap', 'landsat', 'remote sensing'],
+    install_requires=['six', 'requests', 'pylru', 'termcolor', 'nose', 'click', 'DateTime', 'pandas'],
     tests_require=['tox'],
-    cmdclass = {'test': Tox},
+    cmdclass={'test': Tox},
     extras_require={
         'dev': [''],
         'test': ['']
     },
-    entry_points= {
-        'console_scripts': [
-        'lcmap=lcmap.client.scripts.cl_tool.main:main']
+    entry_points={
+        'console_scripts': ['lcmap=lcmap.client.scripts.cl_tool.main:main']
     },
 )
